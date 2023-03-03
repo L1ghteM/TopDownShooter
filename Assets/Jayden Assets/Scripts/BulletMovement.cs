8aeb7dbe4fe58dfc830bf07765c0c668065cc2e2
L1ghteM <jaydenlong25@gmail.com>
2023-03-03 10:09:38 -0800

    Final Push
    Added Background, and borders, music

--- Assets/Jayden Assets/Scripts/BulletMovement.cs
+++ Assets/Jayden Assets/Scripts/BulletMovement.cs
@@ -8,7 +8,6 @@ public class BulletMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -33,3 +32,4 @@ public class BulletMovement : MonoBehaviour
         }
     }
 }
+
